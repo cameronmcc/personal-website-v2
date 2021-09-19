02975ea23e4ada44c55decb3dc0a8272cfbe4213
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-09-19 15:30:20 -0600

    Design is really coming along. Almost ready to modularize into component elements and create data for the projects section.

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import React from "react";
 import svg2 from "../../images/svg2.svg";
+import svg3 from "../../images/svg3.svg";
 import { Link as LinkS } from "react-scroll";
 
 const InfoSection = () => {
@@ -31,7 +32,32 @@ const InfoSection = () => {
           </div>
         </div>
       </section>
+      <section className='info-section-container2'>
+        <div className='info-row'>
+          <div className='info-column'>
+            <div className='text-wrapper'>
+              <div className='top-line' >
+                <div className='heading'>
+                  <div className='subtitle'>
+                    Hi there! I'm Cameron and I'm a full stack developer specialized in using React with the MERN stack. I have also used Spring and Django. Feel free to check out some of my work below.
+
+                  </div>
+                    <a href='projects'>
+                      <button className='btn'>Projects</button>
+                    </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='info-column'>
+            <div className='img-wrapper'>
+              <img className='info-image' src={svg3} alt='alt'></img>
+            </div>
+          </div>
+        </div>
+      </section>
     </container>
+    
   );
 };
 
