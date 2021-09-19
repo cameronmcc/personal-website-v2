9627a6dfe5317f88949ea8a4345de31833587016
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-09-19 14:31:24 -0600

    Got design looking pretty good with flexbox elements looking nice.

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -8,22 +8,25 @@ const InfoSection = () => {
     <container className='info-section'>
       <section className='info-section-container'>
         <div className='info-row'>
-          <div className='infoColumn'>
+          <div className='info-column'>
             <div className='text-wrapper'>
               <div className='top-line'>
+                  Cameron McCloskey
                 <div className='heading'>
+                Welcome to my Personal Portfolio
                   <div className='subtitle'>
+                    Full Stack Developr
+                  </div>
                     <a href='projects'>
                       <button className='btn'>Projects</button>
                     </a>
-                  </div>
                 </div>
               </div>
             </div>
           </div>
           <div className='info-column'>
             <div className='img-wrapper'>
-              <img src={svg2} alt='image1'></img>
+              <img className='info-image' src={svg2} alt='alt'></img>
             </div>
           </div>
         </div>
