cb807165df9be9739cddc1a41cefefc5d11194dc
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-10-04 21:43:11 -0600

    Design is really starting to come together. Added more icons, next up is some conditional rendering to really make the mobile/desktop experiences stand out.

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { DiReact, DiFirebase, DiAptana } from "react-icons/di";
+import { IoLogoNodejs, IoLogoCss3, IoLogoHtml5 } from "react-icons/io"
 import { IconContext } from "react-icons/lib";
 import "./style.css";
 
@@ -9,20 +10,23 @@ const Technologies = () => {
       <div className='technologies-container'>
           <div className='tech-heading'>Technologies</div>
           <section className='tech-items-container'>
-
             <div className='tech-items tech-item-1'>
               <div className='tech-item-row'>
                 <DiReact size='6rem' className='tech-icons' />
+                  <div className='tech-text-wrapper'>
+                    <div className='tech-items-heading'>Front-End</div>
+                    <h3>React</h3>
+                    <h3>CSS</h3>
+                    <h3>HTML</h3>
+                  </div>
               </div>
-              <div className='tech-item-row2'>
-                <div className='tech-text-wrapper'>
-                  <div className='tech-items-heading'>Front-End</div>
-                  <h3>React</h3>
-                  <h3>CSS</h3>
-                  <h3>HTML</h3>
-                </div>
+              <div className='tech-side-icons'>
+              <IoLogoNodejs size='8rem' className='tech-icons' />
+              <IoLogoCss3 size='8rem' className='tech-icons' />
+              <IoLogoHtml5 size='8rem' className='tech-icons' />
               </div>
             </div>
+
             <div className='tech-items tech-item-2'>
               <div className='tech-item-row'>
                 <DiAptana size='6rem' className='tech-icons' />
@@ -32,6 +36,7 @@ const Technologies = () => {
                   <div className='tech-items-heading'>Back-End</div>
                   <h3>Javascript</h3>
                   <h3>Python</h3>
+                  <h3>Node.js</h3>
                 </div>
               </div>
             </div>
@@ -47,10 +52,11 @@ const Technologies = () => {
                   <h3>Django ORM</h3>
                 </div>
               </div>
+              
             </div>
           <div className='empty-grid empty-grid-item-1'></div>
-          <div className='empty-grid empty-grid-item-2'></div>
-          <button className='tech-btn'>Go to Portfolio</button>
+          <div className='empty-grid empty-grid-item-2'></div><div className='tech-btn-container'>
+          <button className='tech-btn'>Go to Portfolio</button></div>
           </section>
 
         </div>
