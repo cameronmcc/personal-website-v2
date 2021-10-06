904277cf4bebde74915b32f015153e18220a9c04
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-10-05 21:14:35 -0600

    Redsign is going well. Mobile first design approach using some advanced css grid layouts. Learning a ton.

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -6,50 +6,50 @@ import { Link as LinkS } from "react-scroll";
 
 const InfoSection = () => {
   return (
-    <container className='info-section-container'>
-      <section className='info-section'>
-        <div className='info-row'>
-          <div className='info-column'>
-            <div className='text-wrapper'>
-              <div className='top-line'>
+    <container className="info-section-container">
+      <section className="info-section">
+        <div className="info-row">
+          <div className="info-column">
+            <div className="text-wrapper">
+              <div className="top-line">
                 Cameron McCloskey
-                <div className='heading'>
+                <div className="heading">
                   Welcome to my Personal Portfolio
-                  <div className='subtitle'>Full Stack Developr</div>
-                  <a href='projects'>
-                    <button className='btn'>Projects</button>
+                  <div className="subtitle">Full Stack Developr</div>
+                  <a href="projects">
+                    <button className="btn">Projects</button>
                   </a>
                 </div>
               </div>
             </div>
           </div>
-          <div className='info-column'>
-            <div className='img-wrapper'>
-              <img className='info-image' src={svg2} alt='alt'></img>
+          <div className="info-column">
+            <div className="img-wrapper">
+              <img className="info-image" src={svg2} alt="alt"></img>
             </div>
           </div>
         </div>
       </section>
-      <section className='info-section-container2'>
-        <div className='info-row'>
-          <div className='info-column'>
-            <div className='img-wrapper'>
-              <img className='info-image' src={svg3} alt='alt'></img>
+      <section className="info-section-2">
+        <div className="info-row">
+          <div className="info-column">
+            <div className="img-wrapper">
+              <img className="info-image" src={svg3} alt="alt"></img>
             </div>
           </div>
-          <div className='info-column'>
-            <div className='text-wrapper'>
-              <div className='top-line'>
-                <div className='heading'>
+          <div className="info-column">
+            <div className="text-wrapper">
+              <div className="top-line">
+                <div className="heading">
                   About Me
-                  <div className='subtitle'>
+                  <div className="subtitle">
                     Hi there! I'm Cameron and I'm a full stack developer
-                    specialized in making React apps using the
-                    MERN Framework. I've also coded in Spring and Django. Feel free
-                    to check out some of my work below.
+                    specialized in making React apps using the MERN Framework.
+                    I've also coded in Spring and Django. Feel free to check out
+                    some of my work below.
                   </div>
-                  <a href='projects'>
-                    <button className='btn2'>Technologies</button>
+                  <a href="projects">
+                    <button className="btn2">Technologies</button>
                   </a>
                 </div>
               </div>
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,66 +1,117 @@
 import React from "react";
 import { DiReact, DiFirebase, DiAptana } from "react-icons/di";
-import { IoLogoNodejs, IoLogoCss3, IoLogoHtml5 } from "react-icons/io"
+import { IoLogoNodejs, IoLogoCss3, IoLogoHtml5 } from "react-icons/io";
 import { IconContext } from "react-icons/lib";
 import "./style.css";
 
 const Technologies = () => {
   return (
     <>
-      <div className='technologies-container'>
-          <div className='tech-heading'>Technologies</div>
-          <section className='tech-items-container'>
-            <div className='tech-items tech-item-1'>
-              <div className='tech-item-row'>
-                <DiReact size='6rem' className='tech-icons' />
-                  <div className='tech-text-wrapper'>
-                    <div className='tech-items-heading'>Front-End</div>
-                    <h3>React</h3>
-                    <h3>CSS</h3>
-                    <h3>HTML</h3>
-                  </div>
-              </div>
-              <div className='tech-side-icons'>
-              <IoLogoNodejs size='8rem' className='tech-icons' />
-              <IoLogoCss3 size='8rem' className='tech-icons' />
-              <IoLogoHtml5 size='8rem' className='tech-icons' />
+      <div className="technologies-container">
+        <div className="tech-heading">Technologies</div>
+        <section className="tech-items-container">
+          <div className="tech-items tech-item-1">
+            <div className="tech-item-row">
+              <DiReact size="6rem" className="tech-icons" />
+              <div className="tech-text-wrapper">
+                <div className="tech-items-heading">Front-End</div>
+                <h3>React</h3>
+                <h3>CSS</h3>
+                <h3>HTML</h3>
               </div>
             </div>
-
-            <div className='tech-items tech-item-2'>
-              <div className='tech-item-row'>
-                <DiAptana size='6rem' className='tech-icons' />
-              </div>
-              <div className='tech-item-row2'>
-                <div className='tech-text-wrapper'>
-                  <div className='tech-items-heading'>Back-End</div>
-                  <h3>Javascript</h3>
-                  <h3>Python</h3>
-                  <h3>Node.js</h3>
-                </div>
+            <div className="tech-side-icons">
+              <IoLogoNodejs
+                className="tech-side-icon-1"
+                size="8rem"
+                className="tech-icons"
+              />
+            </div>
+            <div className="tech-side-icons">
+              <IoLogoCss3
+                className="tech-side-icon-2"
+                size="8rem"
+                className="tech-icons"
+              />
+            </div>
+            <div className="tech-side-icons">
+              <IoLogoHtml5
+                className="tech-side-icon-3"
+                size="8rem"
+                className="tech-icons"
+              />
+            </div>
+          </div>
+          <div className="tech-items tech-item-2">
+            <div className="tech-item-row">
+              <DiAptana size="6rem" className="tech-icons" />
+              <div className="tech-text-wrapper">
+                <div className="tech-items-heading">Back-End</div>
+                <h3>Javascript</h3>
+                <h3>Python</h3>
+                <h3>Node.js</h3>
               </div>
             </div>
-            <div className='tech-items tech-item-3'>
-              <div className='tech-item-row'>
-                <DiFirebase size='6rem' className='tech-icons' />
-              </div>
-              <div className='tech-item-row2'>
-                <div className='tech-text-wrapper'>
-                  <div className='tech-items-heading'>Databases</div>
-                  <h3>MongoDB</h3>
-                  <h3>SQL/NOSQL</h3>
-                  <h3>Django ORM</h3>
-                </div>
-              </div>
-              
+            <div className="tech-side-icons">
+              <IoLogoNodejs
+                className="tech-side-icon-1"
+                size="8rem"
+                className="tech-icons"
+              />
             </div>
-          <div className='empty-grid empty-grid-item-1'></div>
-          <div className='empty-grid empty-grid-item-2'></div><div className='tech-btn-container'>
-          <button className='tech-btn'>Go to Portfolio</button></div>
-          </section>
-
-        </div>
-        <div className='gradient-container-bottom'></div>
+            <div className="tech-side-icons">
+              <IoLogoCss3
+                className="tech-side-icon-2"
+                size="8rem"
+                className="tech-icons"
+              />
+            </div>
+            <div className="tech-side-icons">
+              <IoLogoHtml5
+                className="tech-side-icon-3"
+                size="8rem"
+                className="tech-icons"
+              />
+            </div>
+          </div>
+          <div className="tech-items tech-item-3">
+            <div className="tech-item-row">
+              <DiFirebase size="6rem" className="tech-icons" />
+              <div className="tech-text-wrapper">
+                <div className="tech-items-heading">Databases</div>
+                <h3>MongoDB</h3>
+                <h3>SQL/NOSQL</h3>
+                <h3>Django ORM</h3>
+              </div>
+            </div>
+            <div className="tech-side-icons">
+              <IoLogoNodejs
+                className="tech-side-icon-1"
+                size="8rem"
+                className="tech-icons"
+              />
+            </div>
+            <div className="tech-side-icons">
+              <IoLogoCss3
+                className="tech-side-icon-2"
+                size="8rem"
+                className="tech-icons"
+              />
+            </div>
+            <div className="tech-side-icons">
+              <IoLogoHtml5
+                className="tech-side-icon-3"
+                size="8rem"
+                className="tech-icons"
+              />
+            </div>
+          </div>
+          <div className="tech-btn-container">
+            <button className="tech-btn">Go to Portfolio</button>
+          </div>
+        </section>
+      </div>
+      <div className="gradient-container-bottom"></div>
     </>
   );
 };
