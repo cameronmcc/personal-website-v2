e61edc5a89b25a2a2d3972fff8a09c76784ed5f8
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-10-09 14:17:11 -0600

    More change accepts from beta branch. Main now up to date.

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -1,64 +1,55 @@
-import "./style.css";
-import React from "react";
-import svg2 from "../../images/svg2.svg";
-import svg3 from "../../images/svg3.svg";
-import { Link as LinkS } from "react-scroll";
+import './style.css';
+import React from 'react';
+import svg2 from '../../images/svg2.svg';
+import svg3 from '../../images/svg3.svg';
+import { Link as LinkS } from 'react-scroll';
 
 const InfoSection = () => {
   return (
-    <container className="info-section-container">
-      <section className="info-sections info-section">
-        <div className="info-row">
-          <div className="info-column">
-            <div className="text-wrapper">
-              <div className="top-line">
+    <container className='info-section-container'>
+      <section className='info-sections info-section'>
+        <div className='info-row'>
+          <div className='info-column'>
+            <div className='text-wrapper'>
+              <div className='top-line'>
                 Cameron McCloskey
-                <div className="heading">
+                <div className='heading'>
                   Welcome to my Personal Portfolio
-                  <div className="subtitle">Full Stack Developr</div>
-                  <a href="projects">
-                    <button className="btn">Projects</button>
+                  <div className='subtitle'>Full Stack Developr</div>
+                  <a href='projects'>
+                    <button className='btn'>Projects</button>
                   </a>
                 </div>
               </div>
             </div>
           </div>
-          <div className="info-column">
-            <div className="img-wrapper">
-              <img className="info-image" src={svg2} alt="alt"></img>
+          <div className='info-column'>
+            <div className='img-wrapper'>
+              <img className='info-image' src={svg2} alt='alt'></img>
             </div>
           </div>
         </div>
       </section>
-<<<<<<< HEAD
-          <div className='gradient-container'></div>
-      <section className='info-section-container2'>
+      <section className='info-sections info-section-2'>
         <div className='info-row'>
           <div className='info-column'>
             <div className='img-wrapper'>
               <img className='info-image' src={svg3} alt='alt'></img>
-=======
-      <section className="info-sections info-section-2">
-        <div className="info-row">
-          <div className="info-column">
-            <div className="img-wrapper">
-              <img className="info-image" src={svg3} alt="alt"></img>
->>>>>>> beta
             </div>
           </div>
-          <div className="info-column">
-            <div className="text-wrapper">
-              <div className="top-line">
-                <div className="heading">
+          <div className='info-column'>
+            <div className='text-wrapper'>
+              <div className='top-line'>
+                <div className='heading'>
                   About Me
-                  <div className="subtitle">
+                  <div className='subtitle'>
                     Hi there! I'm Cameron and I'm a full stack developer
                     specialized in making React apps using the MERN Framework.
                     I've also coded in Spring and Django. Feel free to check out
                     some of my work below.
                   </div>
-                  <a href="projects">
-                    <button className="btn2">Technologies</button>
+                  <a href='projects'>
+                    <button className='btn2'>Technologies</button>
                   </a>
                 </div>
               </div>
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   DiReact,
   DiFirebase,
@@ -6,7 +6,7 @@ import {
   DiMongodb,
   DiMysql,
   DiDjango,
-} from "react-icons/di";
+} from 'react-icons/di';
 // import {
 //   IoLogoNodejs,
 //   IoLogoCss3,
@@ -15,77 +15,52 @@ import {
 //   IoLogoJavascript,
 // } from "react-icons/io";
 
-import { IconContext } from "react-icons/lib";
-import "./style.css";
+import { IconContext } from 'react-icons/lib';
+import './style.css';
 
 const Technologies = () => {
   return (
     <>
-<<<<<<< HEAD
-      <div className='gradient-container-top'></div>
       <div className='technologies-container'>
-        <div className='info-white-bg'>
+        <div className='content-wrapper'>
           <div className='tech-heading'>Technologies</div>
           <section className='tech-items-container'>
-            <div className='tech-item-squares-container'>
-              <div className='tech-item-1'>
-              <DiReact size='5rem' classname='tech-icons' />
-              <h1>Front-End</h1>
-              <h3>React</h3>
-              <h3>CSS</h3>
-              <h3>HTML</h3>
-=======
-      <div className="technologies-container">
-        <div className="content-wrapper">
-          <div className="tech-heading">Technologies</div>
-          <section className="tech-items-container">
-            <div className="tech-items tech-item-1">
-              <div className="tech-item-row">
-                <DiReact size="7rem" className="tech-icons" />
-                <div className="tech-text-wrapper">
-                  <div className="tech-items-heading">Front-End</div>
+            <div className='tech-items tech-item-1'>
+              <div className='tech-item-row'>
+                <DiReact size='7rem' className='tech-icons' />
+                <div className='tech-text-wrapper'>
+                  <div className='tech-items-heading'>Front-End</div>
                   <h3>React</h3>
                   <h3>CSS</h3>
                   <h3>HTML</h3>
                 </div>
               </div>
->>>>>>> beta
             </div>
-            <div className="tech-items tech-item-2">
-              <div className="tech-item-row">
-                <DiAptana size="7rem" className="tech-icons" />
-                <div className="tech-text-wrapper">
-                  <div className="tech-items-heading">Back-End</div>
+            <div className='tech-items tech-item-2'>
+              <div className='tech-item-row'>
+                <DiAptana size='7rem' className='tech-icons' />
+                <div className='tech-text-wrapper'>
+                  <div className='tech-items-heading'>Back-End</div>
                   <h3>Javascript</h3>
                   <h3>Python</h3>
                   <h3>Node.js</h3>
                 </div>
               </div>
             </div>
-<<<<<<< HEAD
-            <div className='tech-item-3'>
-              <DiFirebase size='5rem' classname='tech-icons' />
-              <h1>Database{`${'   '}`   }    </h1>
-              <h3>MongoDB</h3>
-              <h3>SQL/NOSQL</h3>
-              <h3>Django ORM</h3>
-=======
-            <div className="tech-items tech-item-3">
-              <div className="tech-item-row">
-                <DiFirebase size="7rem" className="tech-icons" />
-                <div className="tech-text-wrapper">
-                  <div className="tech-items-heading">Databases</div>
+            <div className='tech-items tech-item-3'>
+              <div className='tech-item-row'>
+                <DiFirebase size='7rem' className='tech-icons' />
+                <div className='tech-text-wrapper'>
+                  <div className='tech-items-heading'>Databases</div>
                   <h3>MongoDB</h3>
                   <h3>SQL/NOSQL</h3>
                   <h3>Django ORM</h3>
                 </div>
               </div>
->>>>>>> beta
-            </div>
             </div>
           </section>
-          <div className="tech-btn-container">
-            <button className="tech-btn">Go to Portfolio</button>
+          <div className='tech-btn-container'>
+            <button className='tech-btn'>Go to Portfolio</button>
           </div>
         </div>
       </div>
