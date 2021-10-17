16ab98f59b4139d716dd0f45c2bf090837742d63
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-10-17 15:17:28 -0600

    Added a background video to landing page.

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -1,6 +1,7 @@
 import './style.css';
 import React from 'react';
 import svg2 from '../../images/svg2.svg';
+import heroBg from '../../videos/heroBg.mp4';
 import { Link as LinkS } from 'react-scroll';
 import About from '../About/About';
 
@@ -8,28 +9,34 @@ const InfoSection = () => {
   return (
     <container id='home' className='info-section-container'>
       <section className='info-sections info-section'>
-        <div className='info-row'>
-          <div className='info-column'>
-            <div className='text-wrapper'>
-              <div className='top-line'>
-                Cameron McCloskey
-                <div className='heading'>
-                  Welcome to my Personal Portfolio
-                  <div className='subtitle'>Full Stack Developer</div>
-                  <LinkS to='portfolio' className='btns-link'>
-                    <button className='btn'>Projects</button>
-                  </LinkS>
+        <div className='gradient'>
+          <video className='test-video' autoPlay loop muted alt='video'>
+            <source src={heroBg} type='video/mp4'></source>
+          </video>
+          <div className='info-row'>
+            <div className='info-column'>
+              <div className='text-wrapper'>
+                <div className='top-line'>
+                  Cameron McCloskey
+                  <div className='heading'>
+                    Welcome to my Personal Portfolio
+                    <div className='subtitle'>Full Stack Developer</div>
+                    <LinkS to='portfolio' className='btns-link'>
+                      <button className='btn'>Projects</button>
+                    </LinkS>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className='info-column'>
-            <div className='img-wrapper'>
-              <img className='info-image' src={svg2} alt='alt'></img>
+            <div className='info-column'>
+              <div className='img-wrapper'>
+                <img className='info-image' src={svg2} alt='alt'></img>
+              </div>
             </div>
           </div>
         </div>
       </section>
+
       <About />
     </container>
   );
